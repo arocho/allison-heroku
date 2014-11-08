d2a79e660a4f66a50619eb949255646945378316
Arocho <allygmartinez@gmail.com>
2014-11-08 15:48:17 -0500

    Fresh start

--- app.js
+++ app.js
@@ -1,30 +1,66 @@
-var express = require('express')
-var cool = require('cool-ascii-faces')
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
-var api = '77ts672cxn6wfk';
-var secret = 'GKy8lzvzAUi7secM';
-var callback = 'https://allison.herokuapp.com';
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var Linkedin = require('node-linkedin')(api, secret, callback);
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.set('port', process.env.PORT || 5000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var linkedin = Linkedin.init('my_access_token');
+app.use('/', routes);
+app.use('/users', users);
 
-app.get('/oauth/linkedin', function(req, res) {
-    // This will ask for permisssions etc and redirect to callback url.
-    Linkedin.auth.authorize(res, ['r_fullprofile']);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// error handlers
 
-app.get('/', function(request, response) {
-	var result = linkedin.people.me(function(err, $in) {
-    // Loads the profile of access token owner.
-});
-  response.send(result);
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Hello there! I am listenning in on this port, yo.');
+});
+
+module.exports = app;
